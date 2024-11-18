a2bcc34ff0c579509825d3f85fefae3d7713da85
Moldoveanu Iustin <iustin.moldoveanu@stud.ubbcluj.ro>
2024-11-18 17:00:39 +0200

    Initial commit

---
+++ Moldoveanu_Iustin_Lab7/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Moldoveanu_Iustin_Lab7
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
